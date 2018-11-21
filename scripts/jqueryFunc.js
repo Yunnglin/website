ec1e59c061247d7b6c625dda426254ffcec61c82
Yunlin <mao.looper@qq.com>
2018-11-22 00:34:41 +0800

    add

--- scripts/jqueryFunc.js
+++ scripts/jqueryFunc.js
@@ -1,7 +1,19 @@
 // JavaScript source code
 $(document).ready(function () {
-    $("button").click(function () {
-        $("p").hide();
-        
-    });
+    $("#mobile-nav-taggle").click(function () {
+        var mobileMenu = $("#mobile-menu");
+        var nav_btn = $("#mobile-nav-taggle");
+        if (mobileMenu.hasClass("show-nav")) {
+            setTimeout(function () {
+                mobileMenu.addClass("hide-nav").removeClass("show-nav");
+                nav_btn.addClass("hide-taggle").removeClass("show-taggle");
+            }, 100)
+        }
+        else {
+            setTimeout(function () {
+                mobileMenu.addClass("show-nav").removeClass("hide-nav");
+                nav_btn.addClass("show-taggle").removeClass("hide-taggle");
+            }, 100)
+        }
+    })
 });
